66aaf20060a53fb7ca84c53a2bef6c1bb53a2e6a
hussamayoub <54977904+hussamayoub@users.noreply.github.com>
2023-05-25 08:30:43 +0200

    Add files via upload

--- scripts/ddpg.py
+++ scripts/ddpg.py
@@ -1,148 +1,211 @@
-import tensorflow as tf
 import numpy as np
-import random
-import gym
+import os
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
+from tensorflow.keras.optimizers import Adam
 
 
-class Actor(tf.keras.Model):
-    def __init__(self, state_dim, action_dim, action_bound):
-        super(Actor, self).__init__()
-        self.dense1 = tf.keras.layers.Dense(400, activation='relu')
-        self.dense2 = tf.keras.layers.Dense(300, activation='relu')
-        self.dense3 = tf.keras.layers.Dense(action_dim, activation='tanh')
-        self.action_bound = action_bound
 
-    def call(self, inputs):
-        x = self.dense1(inputs)
-        x = self.dense2(x)
-        x = self.dense3(x)
-        return self.action_bound * x
+class ReplayBuffer:
+    def __init__(self, max_size, input_shape, n_actions):
+        self.mem_size = max_size
+        self.mem_cntr = 0
+        self.state_memory = np.zeros((self.mem_size, *input_shape))
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape))
+        self.action_memory = np.zeros((self.mem_size, n_actions))
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
 
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
 
-class Critic(tf.keras.Model):
-    def __init__(self, state_dim, action_dim):
-        super(Critic, self).__init__()
-        self.dense1 = tf.keras.layers.Dense(400, activation='relu')
-        self.dense2 = tf.keras.layers.Dense(300, activation='relu')
-        self.dense3 = tf.keras.layers.Dense(1)
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
 
-    def call(self, inputs, actions):
-        x = tf.concat([inputs, actions], axis=-1)
-        x = self.dense1(x)
-        x = self.dense2(x)
-        return self.dense3(x)
+        self.mem_cntr += 1
 
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
 
-class DDPG:
-    def __init__(self, state_dim, action_dim, action_bound):
-        self.state_dim = state_dim
-        self.action_dim = action_dim
-        self.action_bound = action_bound
+        batch = np.random.choice(max_mem, batch_size, replace=False)
 
-        self.actor = Actor(state_dim, action_dim, action_bound)
-        self.target_actor = Actor(state_dim, action_dim, action_bound)
-        self.critic = Critic(state_dim, action_dim)
-        self.target_critic = Critic(state_dim, action_dim)
+        states = self.state_memory[batch]
+        states_ = self.new_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        dones = self.terminal_memory[batch]
 
-        self.critic_optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-        self.actor_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
-
-        self.gamma = 0.99
-        self.tau = 0.001
-        self.batch_size = 64
-        self.train_step = 0
-        self.target_update_frequency = 10
-
-        self.update_target_networks()
-
-    def update_target_networks(self):
-        self.target_actor.set_weights(self.actor.get_weights())
-        self.target_critic.set_weights(self.critic.get_weights())
-
-    def train(self, env, num_episodes):
-        for episode in range(num_episodes):
-            state = env.reset()
-            done = False
-            total_reward = 0
-
-            while not done:
-                env.render()
-                action = self.select_action(state)
-                next_state, reward, done, _ = env.step(action)
-
-                self.replay_buffer.append((state, action, reward, next_state, done))
-
-                if len(self.replay_buffer) >= self.batch_size:
-                    self.train_actor_critic()
-
-                state = next_state
-                total_reward += reward
-
-            print(f"Episode {episode + 1}: Total Reward = {total_reward}")
-
-    def select_action(self, state):
-        state = np.expand_dims(state, axis=0)
-        action = self.actor(state)
-        return action.numpy()[0]
-
-    def train_actor_critic(self):
-        # minibatch = random.sample(self.replay_buffer, self.batch_size)
-
-        # states = np.array([transition[0] for transition in minibatch])
-        # actions = np.array([transition[1] for transition in minibatch])
-        # rewards = np.array([transition[2] for transition in minibatch])
-        # next_states = np.array([transition[3] for transition in minibatch])
-        # dones = np.array([transition[4] for transition in minibatch])
-
-        # with tf.GradientTape() as tape:
-        #     target_actions = self.target_actor(next_states)
-        #     target_q_values = self.target_critic(next_states, target_actions)
-        #     targets = rewards + self.gamma * target_q_values * (1 - dones)
-        #     predicted_q_values = self.critic(states, actions)
-        #     critic_loss = tf.reduce_mean(tf.square(targets - predicted_q_values))
-
-        # critic_gradients = tape.gradient(critic_loss, self.critic.trainable_variables)
-        # self.critic_optimizer.apply_gradients(zip(critic_gradients, self.critic.trainable_variables))
-
-        # with tf.GradientTape() as tape:
-        #     predicted_actions = self.actor(states)
-        #     actor_loss = -tf.reduce_mean(self.critic(states, predicted_actions))
-
-        # actor_gradients = tape.gradient(actor_loss, self.actor.trainable_variables)
-        # self.actor_optimizer.apply_gradients(zip(actor_gradients, self.actor.trainable_variables))
-
-        # self.update_target_networks()
-
-        minibatch = random.sample(self.replay_buffer, self.batch_size)
-
-        states = np.array([transition[0] for transition in minibatch])
-        actions = np.array([transition[1] for transition in minibatch])
-        rewards = np.array([transition[2] for transition in minibatch])
-        next_states = np.array([transition[3] for transition in minibatch])
-        dones = np.array([transition[4] for transition in minibatch])
-
-        with tf.GradientTape() as tape:
-            target_actions = self.target_actor(next_states)
-            target_q_values = self.target_critic(next_states, target_actions)
-            targets = rewards + self.gamma * target_q_values * (1 - dones)
-            predicted_q_values = self.critic(states, actions)
-            critic_loss = tf.reduce_mean(tf.square(targets - predicted_q_values))
-
-        critic_gradients = tape.gradient(critic_loss, self.critic.trainable_variables)
-        self.critic_optimizer.apply_gradients(zip(critic_gradients, self.critic.trainable_variables))
-
-        with tf.GradientTape() as tape:
-            predicted_actions = self.actor(states)
-            actor_loss = -tf.reduce_mean(self.critic(states, predicted_actions))
-
-        actor_gradients = tape.gradient(actor_loss, self.actor.trainable_variables)
-        self.actor_optimizer.apply_gradients(zip(actor_gradients, self.actor.trainable_variables))
-
-        # Update target networks with a delay
+        return states, actions, rewards, states_, dones
         
-        self.update_target_networks()
+ 
+     
 
-        # Decay exploration noise
-        # exploration_noise *= self.exploration_decay_factor
 
-        self.train_step += 1
+class CriticNetwork(keras.Model):
+    def __init__(self, fc1_dims=512, fc2_dims=512,
+            name='critic', chkpt_dir='tmp/ddpg'):
+        super(CriticNetwork, self).__init__()
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+
+        self.model_name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, 
+                    self.model_name+'_ddpg.h5')
+
+        self.fc1 = Dense(self.fc1_dims, activation='relu')
+        self.fc2 = Dense(self.fc2_dims, activation='relu')
+        self.q = Dense(1, activation=None)
+
+    def call(self, state, action):
+        action_value = self.fc1(tf.concat([state, action], axis=1))
+        action_value = self.fc2(action_value)
+
+        q = self.q(action_value)
+
+        return q
+
+class ActorNetwork(keras.Model):
+    def __init__(self, fc1_dims=512, fc2_dims=512, n_actions=2, name='actor',
+            chkpt_dir='tmp/ddpg'):
+        super(ActorNetwork, self).__init__()
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+
+        self.model_name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, 
+                    self.model_name+'_ddpg.h5')
+
+        self.fc1 = Dense(self.fc1_dims, activation='relu')
+        self.fc2 = Dense(self.fc2_dims, activation='relu')
+        self.mu = Dense(self.n_actions, activation='tanh')
+
+    def call(self, state):
+        prob = self.fc1(state)
+        prob = self.fc2(prob)
+
+        mu = self.mu(prob)
+
+        return mu
+     
+     
+     
+     
+   
+
+
+
+class Agent:
+    def __init__(self, input_dims, alpha=0.001, beta=0.002, env=None,
+                 gamma=0.99, n_actions=2, max_size=1000000, tau=0.005,
+                 fc1=400, fc2=300, batch_size=64, noise=0.1):
+        self.gamma = gamma
+        self.tau = tau
+        self.memory = ReplayBuffer(max_size, input_dims, n_actions)
+        self.batch_size = batch_size
+        self.n_actions = n_actions
+        self.noise = noise
+        self.max_action = env.action_space.high[0]
+        self.min_action = env.action_space.low[0]
+
+        self.actor = ActorNetwork(n_actions=n_actions, name='actor')
+        self.critic = CriticNetwork(name='critic')
+        self.target_actor = ActorNetwork(n_actions=n_actions,
+                                         name='target_actor')
+        self.target_critic = CriticNetwork(name='target_critic')
+
+        self.actor.compile(optimizer=Adam(learning_rate=alpha))
+        self.critic.compile(optimizer=Adam(learning_rate=beta))
+        self.target_actor.compile(optimizer=Adam(learning_rate=alpha))
+        self.target_critic.compile(optimizer=Adam(learning_rate=beta))
+
+        self.update_network_parameters(tau=1)
+
+    def update_network_parameters(self, tau=None):
+        if tau is None:
+            tau = self.tau
+
+        weights = []
+        targets = self.target_actor.weights
+        for i, weight in enumerate(self.actor.weights):
+            weights.append(weight * tau + targets[i]*(1-tau))
+        self.target_actor.set_weights(weights)
+
+        weights = []
+        targets = self.target_critic.weights
+        for i, weight in enumerate(self.critic.weights):
+            weights.append(weight * tau + targets[i]*(1-tau))
+        self.target_critic.set_weights(weights)
+
+    def remember(self, state, action, reward, new_state, done):
+        self.memory.store_transition(state, action, reward, new_state, done)
+
+    def save_models(self):
+        print('... saving models ...')
+        self.actor.save_weights(self.actor.checkpoint_file)
+        self.target_actor.save_weights(self.target_actor.checkpoint_file)
+        self.critic.save_weights(self.critic.checkpoint_file)
+        self.target_critic.save_weights(self.target_critic.checkpoint_file)
+
+    def load_models(self):
+        print('... loading models ...')
+        self.actor.load_weights(self.actor.checkpoint_file)
+        self.target_actor.load_weights(self.target_actor.checkpoint_file)
+        self.critic.load_weights(self.critic.checkpoint_file)
+        self.target_critic.load_weights(self.target_critic.checkpoint_file)
+
+    def choose_action(self, observation, evaluate=False):
+        state = tf.convert_to_tensor([observation], dtype=tf.float32)
+        actions = self.actor(state)
+        if not evaluate:
+            actions += tf.random.normal(shape=[self.n_actions],
+                                        mean=0.0, stddev=self.noise)
+        # note that if the env has an action > 1, we have to multiply by
+        # max action at some point
+        actions = tf.clip_by_value(actions, self.min_action, self.max_action)
+
+        return actions[0]
+
+    def learn(self):
+        if self.memory.mem_cntr < self.batch_size:
+            return
+
+        state, action, reward, new_state, done = \
+            self.memory.sample_buffer(self.batch_size)
+
+        states = tf.convert_to_tensor(state, dtype=tf.float32)
+        states_ = tf.convert_to_tensor(new_state, dtype=tf.float32)
+        rewards = tf.convert_to_tensor(reward, dtype=tf.float32)
+        actions = tf.convert_to_tensor(action, dtype=tf.float32)
+
+        with tf.GradientTape() as tape:
+            target_actions = self.target_actor(states_)
+            critic_value_ = tf.squeeze(self.target_critic(
+                                states_, target_actions), 1)
+            critic_value = tf.squeeze(self.critic(states, actions), 1)
+            target = rewards + self.gamma*critic_value_*(1-done)
+            critic_loss = keras.losses.MSE(target, critic_value)
+
+        critic_network_gradient = tape.gradient(critic_loss,
+                                                self.critic.trainable_variables)
+        self.critic.optimizer.apply_gradients(zip(
+            critic_network_gradient, self.critic.trainable_variables))
+
+        with tf.GradientTape() as tape:
+            new_policy_actions = self.actor(states)
+            actor_loss = -self.critic(states, new_policy_actions)
+            actor_loss = tf.math.reduce_mean(actor_loss)
+
+        actor_network_gradient = tape.gradient(actor_loss,
+                                               self.actor.trainable_variables)
+        self.actor.optimizer.apply_gradients(zip(
+            actor_network_gradient, self.actor.trainable_variables))
+
+        self.update_network_parameters()
--- scripts/simulation.py
+++ scripts/simulation.py
@@ -1,35 +1,66 @@
-#export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+import gym
+import numpy as np
+from ddpg import *
 import pygame
 import math
 import numpy as np
 from constants import *
 from environment import Environment
 import time
-from ddpg import DDPG
 import tensorflow as tf
 import gym
 
 
+if __name__ == '__main__':
+    env = Environment()
+    agent = Agent(input_dims=env.observation_space.shape, env=env,
+            n_actions=env.action_space.shape[0])
+    n_games = 1000
 
 
+    best_score = env.reward_range[0]
+    score_history = []
+    load_checkpoint = False
 
-if __name__ == "__main__":
-    pygame.init()
-    env = Environment() 
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0]
-    action_bound = env.action_space.high
+    if load_checkpoint:
+        n_steps = 0
+        while n_steps <= agent.batch_size:
+            observation = env.reset()
+            action = env.action_space.sample()
+            observation_, reward, done, info = env.step(action)
+            agent.remember(observation, action, reward, observation_, done)
+            n_steps += 1
+        agent.learn()
+        agent.load_models()
+        evaluate = True
+    else:
+        evaluate = False
 
-    agent = DDPG(state_dim, action_dim, action_bound)
-    agent.replay_buffer = []
-    agent.train(env, num_episodes=10000000)
-    
-    # # Usage example
-    # env = gym.make('LunarLanderContinuous-v2')
-    # state_dim = env.observation_space.shape[0]
-    # action_dim = env.action_space.shape[0]
-    # action_bound = env.action_space.high
+    for i in range(n_games):
+        observation = env.reset()
+        done = False
+        score = 0
+        while not done:
+            pygame.display.update()
+            action = agent.choose_action(observation, evaluate)
+            # print(action.numpy())
+            
+            observation_, reward, done, info = env.step(action.numpy())
+            score += reward
+            agent.remember(observation, action, reward, observation_, done)
+            if not load_checkpoint:
+                agent.learn()
+            observation = observation_
 
-    # agent = DDPG(state_dim, action_dim, action_bound)
-    # agent.replay_buffer = []
-    # agent.train(env, num_episodes=100)
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        # if avg_score > best_score:
+        #     best_score = avg_score
+        #     if not load_checkpoint:
+                # agent.save_models()
+
+        print('episode ', i, 'score %.1f' % score, 'avg score %.1f' % avg_score)
+
+    if not load_checkpoint:
+        x = [i+1 for i in range(n_games)]
